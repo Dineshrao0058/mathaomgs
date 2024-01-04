e6ba804c27072f8632c02a01dd1ff3034b744c2a
Dineshrao0058 <dineshrao.vita@gmail.com>
2024-01-04 11:14:10 +0530

    upload done

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  uploadFile: any;
+  showImg: boolean = false;
+  hideFile: boolean = true;
+  viewfile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var image1 = new FileReader();
+      image1.readAsDataURL(event.target.files[0]);
+      image1.onload = (event) => {
+        this.uploadFile = '';
+        this.uploadFile = event.target?.result
+        this.hideFile = false;
+        this.showImg = true;
 
+        console.log(this.uploadFile);
+      }
+
+    }
+
+  }
 }
+
+
