571f6914df2a4a90b578e4086ce220074da6a79b
Dineshrao0058 <dineshrao.vita@gmail.com>
2024-01-04 12:32:20 +0530

    upload done

--- src/app/wallclock/wallclock.component.ts
+++ src/app/wallclock/wallclock.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./wallclock.component.scss']
 })
 export class WallclockComponent {
+  uploadFile: any;
+  showImg: boolean = false;
+  hideFile: boolean = true;
+  viewfile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var image1 = new FileReader();
+      image1.readAsDataURL(event.target.files[0]);
+      image1.onload = (event) => {
+        this.uploadFile = '';
+        this.uploadFile = event.target?.result
+        this.hideFile = false;
+        this.showImg = true;
 
+        console.log(this.uploadFile);
+      }
+    }
+  }
 }
