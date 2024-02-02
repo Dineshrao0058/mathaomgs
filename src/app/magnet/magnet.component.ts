1805162454986a7517161ab2bc9b538412f225ee
Dineshrao0058 <dineshrao.vita@gmail.com>
2024-02-02 15:56:37 +0530

    changes in magnet

--- src/app/magnet/magnet.component.ts
+++ src/app/magnet/magnet.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-magnet',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./magnet.component.scss']
 })
 export class MagnetComponent {
+  ngSwitch!: any;
+  uploadFile: any;
+  showImg: boolean = false;
+  hideFile: boolean = true;
+  btns: boolean = false;
+  btn: boolean = false;
+  viewfile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var image1 = new FileReader();
+      image1.readAsDataURL(event.target.files[0]);
+      image1.onload = (event) => {
+        this.uploadFile = '';
+        this.uploadFile = event.target?.result
+        this.hideFile = false;
+        this.showImg = true;
 
+        console.log(this.uploadFile);
+      }
+    }
+  }
+  disabledbutton() {
+    this.btns = true
+  }
+  disablebutton() {
+    this.btn = true
+  }
 }
+
