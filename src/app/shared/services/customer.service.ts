7cf9f4ee0a94856af7ce4c1e5cf60b0b34e25f7b
srikanthyaswada <srikanthyaswada.vita@gmail.com>
2024-02-28 15:56:27 +0530

    merge to master

--- src/app/shared/services/customer.service.ts
+++ src/app/shared/services/customer.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
- 
+
 @Injectable({
   providedIn: 'root',
 })
@@ -77,7 +77,10 @@ export class CustomerService {
     );
   }
 
-  getAddress(id:any) {
-    return this.http.get('http://localhost:5000/cart/viewCheckoutbyId/'+id, this.jwttoken());
+  getAddress(id: any) {
+    return this.http.get(
+      'http://localhost:5000/cart/viewCheckoutbyId/' + id,
+      this.jwttoken()
+    );
   }
 }
