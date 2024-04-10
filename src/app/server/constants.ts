26a319e900dbf7738e309646156f65e6df2b0a32
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-04-10 14:18:20 +0530

    local url change

--- src/app/server/constants.ts
+++ src/app/server/constants.ts
@@ -1,8 +1,8 @@
 export const CONSTANTS = {
-    serverUrl: 'http://78.142.47.247:8000',
-    customerUrl: 'http://78.142.47.247:8000/customer',
-    cartUrl: 'http://78.142.47.247:8000/cart',
-    sizeUrl: 'http://78.142.47.247:8000/size/getsizes',
-    thicknessUrl: 'http://78.142.47.247:8000/thickness/getAllthickness',
-    priceUrl: 'http://78.142.47.247:8000/price/getpricesWithid',
+    serverUrl: 'http://localhost:8000',
+    customerUrl: 'http://localhost:8000/customer',
+    cartUrl: 'http://localhost:8000/cart',
+    sizeUrl: 'http://localhost:8000/size/getsizes',
+    thicknessUrl: 'http://localhost:8000/thickness/getAllthickness',
+    priceUrl: 'http://localhost:8000/price/getpricesWithid',
 }
